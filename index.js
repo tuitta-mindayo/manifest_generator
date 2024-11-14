6aecfb643643586246f90c4e7e98c60aa18e2e30
びゅな <153439719+haru3776@users.noreply.github.com>
2024-11-14 13:01:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("みなさんこんにちは！");
+//alert("みなさんこんにちは！");
+const button = document.getElementById('button');
+button.addEventListener('click', pushButton1);
+//button.addEventListener('click', pushButton2);
+
+function pushButton1() {
+  alert('クリックされました -1-');
+}
+/*
+function pushButton2() {
+  alert('クリックされました -2-');
+}
+*/
