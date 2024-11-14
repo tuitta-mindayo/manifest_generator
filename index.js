9ecb4e30d6e0c9221c3df98aa0d1870cd6521c5f
びゅな <153439719+haru3776@users.noreply.github.com>
2024-11-14 13:26:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import {fs} from 'fs';
 const data = JSON.parse(fs.readFileSync("/manifest.json", 'utf8'));
-fs.writeFileSync(RESULT_FILE, JSON.stringify(data));
-fs.writeFileSync(RESULT_FILE, JSON.stringify(data));
+//fs.writeFileSync(RESULT_FILE, JSON.stringify(data));
+//fs.writeFileSync(RESULT_FILE, JSON.stringify(data));
 const button = document.getElementById('button');
 button.addEventListener('click', pushButton1);
 button.addEventListener('click', pushButton2);
 
 function pushButton1() {
-  alert('クリックされました -1-');
+  alert(JSON.stringify(data));
 }
 
 function pushButton2() {
