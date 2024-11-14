33943341e73f0d343f8fc670532e2afdad5a73d6
びゅな <153439719+haru3776@users.noreply.github.com>
2024-11-14 13:49:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-const manifest = `{
-"format_version": 2,
-    "header": {
-        "description": "アドオンの説明文",
-        "name": "アドオンの名前",
-        "uuid": "uuid1",
-        "version": [0, 0, 1],
-        "min_engine_version": [ 1, 21, 50 ]
-    },
-    "modules": [
-        {
-            "description": "アドオンの説明文",
-            "type": "resources",
-            "uuid": "uuid2",
-            "version": [0, 0, 1]
-        }
-    ]
-}`;
+import { rManifest } from "./data.js"
 const button = document.getElementById('button');
 button.addEventListener('click', pushButton1);
 button.addEventListener('click', pushButton2);
@@ -25,6 +8,6 @@ function pushButton1() {
 }
 
 function pushButton2() {
-  alert('クリックされました -2-');
+  alert('click2');
 }
 
