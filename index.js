4cd0012aa3f6fea89c9b2bb89a2d318b3c750d1e
びゅな <153439719+haru3776@users.noreply.github.com>
2024-11-14 13:25:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+import {fs} from 'fs';
+const data = JSON.parse(fs.readFileSync("/manifest.json", 'utf8'));
+fs.writeFileSync(RESULT_FILE, JSON.stringify(data));
+fs.writeFileSync(RESULT_FILE, JSON.stringify(data));
 const button = document.getElementById('button');
 button.addEventListener('click', pushButton1);
 button.addEventListener('click', pushButton2);
