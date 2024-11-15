cc8a94079288c413ef861c7def2a33d99987237f
びゅな <153439719+haru3776@users.noreply.github.com>
2024-11-15 11:37:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-import { rManifest } from "./data.js"
-const button = document.getElementById('button');
-button.addEventListener('click', pushButton1);
-button.addEventListener('click', pushButton2);
+//import data
+import { rManifest } from "./data.js";
 
-function pushButton1() {
-  alert('click1');
+//getElements
+const name = document.getElementById('name');
+const description = document.getElementById('description');
+const addonType = document.getElementById('addonType');
+const minVersion = document.getElementById('minVersion');
+const make = document.getElementById('make');
+
+//EventListeners
+make.addEventListener('click', makeFile);
+
+//function
+function makeFile() {
+  if(name.value != "" && description.value != "")
+  {
+    //make array
+    let version = minVersion.select.value.split(".");
+
+    //update json
+    let jsonData = JSON.parse(rManifest);
+    
+    //header
+    jsonData.header.name = name.value;
+    jsonData.header.description = description.value;
+    jsonData.header.uuid = crypto.randomUUID();
+    jsonData.header.min_engine_version[0] = parseInt(version[0],10);
+    jsonData.header.min_engine_version[1] = parseInt(version[1],10);
+    jsonData.header.min_engine_version[2] = parseInt(version[2],10);
+
+    //modules
+    jsonData.modules[0].description = description.value;
+    jsonData.modules[0].type = addonType.select.value;
+    jsonData.modules[0].uuid = crypto.randomUUID();
+
+    //export manifest.json
+    const json = JSON.stringify(jsonData, null, '  ');
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const linkTag = document.createElement('a');
+    linkTag.href = url;
+    linkTag.download = 'manifest.json';
+    linkTag.click()
+
+    URL.revokeObjectURL(url)
+    linkTag.remove()
+  }
+  else
+  {
+    alert("アドオン名かアドオン説明文が設定されていません。");
+  }
 }
-
-function pushButton2() {
-  alert('click2');
-}
-
