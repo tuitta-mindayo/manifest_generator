a4a522bf3a5af6dd6a492c0d344d2a76d229dd94
びゅな <153439719+tuitta-mindayo@users.noreply.github.com>
2024-12-18 09:35:20 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
-const rManifest = `{
+const normalManifest = `{
     "format_version": 2,
     "header": {
         "description": "アドオンの説明文",
         "name": "アドオンの名前",
         "uuid": "uuid1",
         "version": [0, 0, 1],
-        "min_engine_version": [ 1, 21, 50 ]
+        "min_engine_version": [ 1, 21, 10 ]
     },
     "modules": [
         {
@@ -17,4 +17,34 @@ const rManifest = `{
     ]
 }`;
 
-export {rManifest}
+const scriptManifest = `{
+    "format_version": 2,
+    "header": {
+        "description": "アドオンの説明文",
+        "name": "アドオンの名前",
+        "uuid": "uuid1",
+        "version": [0, 0, 1],
+        "min_engine_version": [ 1, 21, 10 ]
+    },
+    "modules": [
+        {
+            "description": "アドオンの説明文",
+            "type": "script",
+            "entry": "scripts/index.js",
+            "uuid": "uuid2",
+            "version": [0, 0, 1]
+        }
+    ],
+    "dependencies": [
+        {
+            "module_name": "@minecraft/server",
+            "version": "1.17.0-beta"
+        },
+        {
+            "module_name": "@minecraft/server-ui",
+            "version": "1.3.0"
+        }
+    ]
+}`;
+
+export {normalManifest, scriptManifest}
